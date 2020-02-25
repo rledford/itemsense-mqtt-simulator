ad965df9d886ce62f38ec209ce1be55fc202aee5
rledford <ryan.c.ledford@gmail.com>
2020-02-24 22:25:39 -0600

    added sim

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 const { Configuration } = require('./lib/configuration');
+const { Simulator } = require('./lib/simulator');
 
 const config = new Configuration('./config.json');
+const sim = new Simulator(config);
+
+sim.start();
